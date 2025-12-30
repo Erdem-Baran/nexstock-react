6371d680eb1222d96e40a27d01ca2cb7cb1b9d53
Erdem-Baran <erdembaran638@gmail.com>
2025-12-30 12:16:16 +0300

    "API settings have been changed"

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,9 +1,20 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: baseURL,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API Error:", error);
+    return Promise.reject(error);
+  }
+);
+
 export default axiosClient;

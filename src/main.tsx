529da536851f14ec8bed2050624bc280bf25ee9c
Erdem-Baran <erdembaran638@gmail.com>
2026-01-05 14:00:26 +0300

    "Feat: application is wrapped with AuthProvider."

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import { Provider } from 'react-redux'
-import { store } from './store/store.ts'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
+import { AuthProvider } from "./hooks/useAuth.tsx";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
